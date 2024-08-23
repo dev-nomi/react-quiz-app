efc3642737f4f3a9ce7f807045908efafc47749b
dev-nomi <noman.khalid.freelance@gmail.com>
2024-08-23 21:02:30 +0500

    Add shuffling anwswes logic

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import quizCompletedImg from "../assets/quiz-complete.png";
 import QUESTIONS from "../question.js";
 
 export default function Quiz() {
   const [userAnswers, setUserAnwers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
+  const isQuizCompleted = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnwers((prevUserAnwers) => {
@@ -12,12 +14,24 @@ export default function Quiz() {
     });
   }
 
+  if (isQuizCompleted) {
+    return (
+      <div id="summary">
+        <img src={quizCompletedImg} alt="Quiz completed" />
+        <h2>Quiz Completed</h2>
+      </div>
+    );
+  }
+
+  const shuffelAnswers = QUESTIONS[activeQuestionIndex].answers;
+  shuffelAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffelAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
